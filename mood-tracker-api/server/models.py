f08e16e2f35ab1a733387f17fd4898b371cb5168
Mark Kamau <mark.wainaina@student.moringaschool.com>
2026-08-01 21:21:46 +0300

    define User model with username and password hash fields

--- mood-tracker-api/server/models.py
+++ mood-tracker-api/server/models.py
@@ -5,3 +5,12 @@ from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False, unique=True)
+    _password_hash = db.Column(db.String, nullable=False)
+    
